82b399e538d51032ce052c0b8859765b8dd47526
mfdii <michael@opscode.com>
2014-09-03 14:20:16 -0400

    change haproxy attributes

--- roles/LoadBalancer.rb
+++ roles/LoadBalancer.rb
@@ -1,3 +1,12 @@
 name "LoadBalancer"
 description "Sets up HAProxy to balance between servers with role[webserver]"
 run_list "role[base_centos]","recipe[haproxy::app_lb]","recipe[haproxy]"
+default_attributes(
+	:haproxy => {
+		:member_port => 80,
+		:admin => {
+			:address_bind => "0.0.0.0"
+		}
+	}
+
+)
