87069db521c221dec9bb195c8591059a2b28879b
mfdii <michael@opscode.com>
2014-09-03 15:03:07 -0400

    enable admin port of haproxy

--- roles/LoadBalancer.rb
+++ roles/LoadBalancer.rb
@@ -2,5 +2,7 @@ name "LoadBalancer"
 description "Sets up HAProxy to balance between servers with role[webserver]"
 run_list "role[base_centos]","recipe[lb]"
 default_attributes(
-	
+	:fwrules => {
+		:'22002' => true
+	}
 )
