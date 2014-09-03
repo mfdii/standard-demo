ce49c6cd5531e1f550a081e0912e2d2df5bf84fd
mfdii <michael@opscode.com>
2014-09-03 12:43:26 -0400

    make apache depend on iptables

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,7 +1,9 @@
 name             'apache'
-maintainer       'Infosys'
-maintainer_email 'swaroop_g02@infosys.com'
+maintainer       'Chef'
+maintainer_email 'michael@getchef.com'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+
+depends 'iptables'
