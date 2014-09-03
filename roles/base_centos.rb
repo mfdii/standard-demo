e9d5fd243aeab2a8c73dbc199667f6e2b52e1e6d
mfdii <michael@opscode.com>
2014-09-03 11:25:40 -0400

    Add Centos Role and cookbooks

---
+++ roles/base_centos.rb
@@ -0,0 +1,11 @@
+name "base_centos"
+description "Base role for Centos nodes"
+run_list "recipe[chef-client]","recipe[push-jobs]"
+default_attributes(
+	:chef_client => {
+		:interval => 30
+	},
+	:push_jobs	=> {
+		:package_url => "https://s3.amazonaws.com/opscode-private-chef/el/6/x86_64/opscode-push-jobs-client-1.0.2-1.el6.x86_64.rpm"
+	}
+)
