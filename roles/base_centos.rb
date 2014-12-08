241ae5ac877befd4db73847c81e638953bf90f2e
mfdii <michael@opscode.com>
2014-12-08 12:51:37 -0500

    update roles push-jobs-urls

--- roles/base_centos.rb
+++ roles/base_centos.rb
@@ -8,7 +8,6 @@ default_attributes(
 	},
 	:push_jobs	=> {
 		:package_url => "https://opscode-private-chef.s3.amazonaws.com/el/6/x86_64/opscode-push-jobs-client-1.1.5-1.el6.x86_64.rpm",
-		:package_checksum => "08d1e8058e2ca1290f71f9c2ba6684cecfc2099d"
 	},
 	:iptables => {
 		:install_rules => false
